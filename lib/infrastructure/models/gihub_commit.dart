dc6eecc61d6fdc87e86dc922eb229aea3c79bc5c
Josue Lemus Miranda <josuelemusmiranda@MacBook-Pro-de-Josue.local>
2024-02-01 17:38:27 -0400

    Create github model for responses from github api

---
+++ lib/infrastructure/models/gihub_commit.dart
@@ -0,0 +1,237 @@
+class GithubCommit {
+  String sha;
+  String nodeId;
+  Commit commit;
+  CommitAuthor author;
+  CommitAuthor committer;
+
+  GithubCommit({
+    required this.sha,
+    required this.nodeId,
+    required this.commit,
+    required this.author,
+    required this.committer,
+  });
+
+  factory GithubCommit.fromJson(Map<String, dynamic> json) => GithubCommit(
+        sha: json["sha"],
+        nodeId: json["node_id"],
+        commit: Commit.fromJson(json["commit"]),
+        author: CommitAuthor.fromJson(json["author"]),
+        committer: CommitAuthor.fromJson(json["committer"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "sha": sha,
+        "node_id": nodeId,
+        "commit": commit.toJson(),
+        "author": author.toJson(),
+        "committer": committer.toJson(),
+      };
+
+  static List<GithubCommit> fromJsonList(List<dynamic> json) {
+    final complaints = json.map((data) => GithubCommit.fromJson(data)).toList();
+    return complaints;
+  }
+}
+
+class CommitAuthor {
+  String login;
+  int id;
+  String nodeId;
+  String avatarUrl;
+  String gravatarId;
+  String url;
+  String htmlUrl;
+  String followersUrl;
+  String followingUrl;
+  String gistsUrl;
+  String starredUrl;
+  String subscriptionsUrl;
+  String organizationsUrl;
+  String reposUrl;
+  String eventsUrl;
+  String receivedEventsUrl;
+  String type;
+  bool siteAdmin;
+
+  CommitAuthor({
+    required this.login,
+    required this.id,
+    required this.nodeId,
+    required this.avatarUrl,
+    required this.gravatarId,
+    required this.url,
+    required this.htmlUrl,
+    required this.followersUrl,
+    required this.followingUrl,
+    required this.gistsUrl,
+    required this.starredUrl,
+    required this.subscriptionsUrl,
+    required this.organizationsUrl,
+    required this.reposUrl,
+    required this.eventsUrl,
+    required this.receivedEventsUrl,
+    required this.type,
+    required this.siteAdmin,
+  });
+
+  factory CommitAuthor.fromJson(Map<String, dynamic> json) => CommitAuthor(
+        login: json["login"],
+        id: json["id"],
+        nodeId: json["node_id"],
+        avatarUrl: json["avatar_url"],
+        gravatarId: json["gravatar_id"],
+        url: json["url"],
+        htmlUrl: json["html_url"],
+        followersUrl: json["followers_url"],
+        followingUrl: json["following_url"],
+        gistsUrl: json["gists_url"],
+        starredUrl: json["starred_url"],
+        subscriptionsUrl: json["subscriptions_url"],
+        organizationsUrl: json["organizations_url"],
+        reposUrl: json["repos_url"],
+        eventsUrl: json["events_url"],
+        receivedEventsUrl: json["received_events_url"],
+        type: json["type"],
+        siteAdmin: json["site_admin"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "login": login,
+        "id": id,
+        "node_id": nodeId,
+        "avatar_url": avatarUrl,
+        "gravatar_id": gravatarId,
+        "url": url,
+        "html_url": htmlUrl,
+        "followers_url": followersUrl,
+        "following_url": followingUrl,
+        "gists_url": gistsUrl,
+        "starred_url": starredUrl,
+        "subscriptions_url": subscriptionsUrl,
+        "organizations_url": organizationsUrl,
+        "repos_url": reposUrl,
+        "events_url": eventsUrl,
+        "received_events_url": receivedEventsUrl,
+        "type": type,
+        "site_admin": siteAdmin,
+      };
+}
+
+class Commit {
+  CommitAuthorClass author;
+  CommitAuthorClass committer;
+  String message;
+  Tree tree;
+  String url;
+  int commentCount;
+
+  Commit({
+    required this.author,
+    required this.committer,
+    required this.message,
+    required this.tree,
+    required this.url,
+    required this.commentCount,
+  });
+
+  factory Commit.fromJson(Map<String, dynamic> json) => Commit(
+        author: CommitAuthorClass.fromJson(json["author"]),
+        committer: CommitAuthorClass.fromJson(json["committer"]),
+        message: json["message"],
+        tree: Tree.fromJson(json["tree"]),
+        url: json["url"],
+        commentCount: json["comment_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "author": author.toJson(),
+        "committer": committer.toJson(),
+        "message": message,
+        "tree": tree.toJson(),
+        "url": url,
+        "comment_count": commentCount,
+      };
+}
+
+class CommitAuthorClass {
+  String name;
+  String email;
+  DateTime date;
+
+  CommitAuthorClass({
+    required this.name,
+    required this.email,
+    required this.date,
+  });
+
+  factory CommitAuthorClass.fromJson(Map<String, dynamic> json) =>
+      CommitAuthorClass(
+        name: json["name"],
+        email: json["email"],
+        date: DateTime.parse(json["date"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "name": name,
+        "email": email,
+        "date": date.toIso8601String(),
+      };
+}
+
+class Tree {
+  String sha;
+  String url;
+
+  Tree({
+    required this.sha,
+    required this.url,
+  });
+
+  factory Tree.fromJson(Map<String, dynamic> json) => Tree(
+        sha: json["sha"],
+        url: json["url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "sha": sha,
+        "url": url,
+      };
+}
+
+class Parent {
+  String sha;
+  String url;
+  String htmlUrl;
+
+  Parent({
+    required this.sha,
+    required this.url,
+    required this.htmlUrl,
+  });
+
+  factory Parent.fromJson(Map<String, dynamic> json) => Parent(
+        sha: json["sha"],
+        url: json["url"],
+        htmlUrl: json["html_url"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "sha": sha,
+        "url": url,
+        "html_url": htmlUrl,
+      };
+}
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
