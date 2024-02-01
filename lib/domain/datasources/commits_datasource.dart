353e73ebcb66b27cae2b40d885f05052a5578887
Josue Lemus Miranda <josuelemusmiranda@MacBook-Pro-de-Josue.local>
2024-02-01 18:08:15 -0400

    Create commit repository in domain layer

--- lib/domain/datasources/commits_datasource.dart
+++ lib/domain/datasources/commits_datasource.dart
@@ -1,6 +1,6 @@
 import 'package:github_commit_viewer/domain/entities/commit.dart';
 
-abstract class CommitDatasource {
+abstract class CommitsDatasource {
   Future<List<Commit>> getCommitList(String owner, String repo, int page,
       {int perPage = 10});
 }
