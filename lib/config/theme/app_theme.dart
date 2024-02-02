201b6de9303d5be1f705580661a60da7f084ca42
JosueLemus <josueunueve@gmail.com>
2024-02-02 12:07:33 -0400

    Update theme styles

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  ThemeData getTheme() =>
-      ThemeData(useMaterial3: true, brightness: Brightness.dark);
+  ThemeData getTheme() => ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: const Color(0xff24292e),
+        appBarTheme: const AppBarTheme(centerTitle: false),
+      );
 }
